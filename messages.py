ad699ce0af49fd70a7bac35465a9914735d0a3af
jpsch8 <johnpaulx@icloud.com>
2024-11-17 16:40:35 -0500

    Update messages.py
    additional documentation

--- messages.py
+++ messages.py
@@ -2,9 +2,11 @@
 import time
 from time import sleep
 import cmath
-#establishes complex variable
+#establishes complex variable (a+bi)
 
+# real part (always 1/2)
 x = 0.5
+# compley part
 y = 0
 z = complex(x,y)
 
@@ -15,6 +17,7 @@ while 1==1:
         sum = sum+(1/(i**z))
 
     print(sum)
+    #increments complext part
     y = y+0.000001
     sleep(1)
 
