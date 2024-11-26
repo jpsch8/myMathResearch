328787be82ecc2e6ef1e8c8f9ecb862e5cbc8bdc
John Paul <johnpaulx@icloud.com>
2024-11-26 08:33:55 -0500

    fixed spelling errors

--- zetaFunction.py
+++ zetaFunction.py
@@ -4,11 +4,11 @@
 import cmath
 #establishes complex variable (a+bi)
 
-# real part (always 1/2)
+
 
 # complex part
 y = 0
-z = complex(0.5,y)
+z = complex(0.5,y) # real part (always 1/2) (or so we think)
 
 sum  = 0
 while True:
@@ -18,7 +18,7 @@ while True:
         sum = sum + ((-1)**(i-1))/(i**z)
 
     
-    # increments complext part
+    # increments complex part
     y = y+0.000001
     #checks if z is a zero (or really close to 0)
     if abs(sum)<0.000001:
