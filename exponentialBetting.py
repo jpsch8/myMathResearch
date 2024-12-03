ad9ec4d2a112c861744d862887eb54782a62b285
John Paul <johnpaulx@icloud.com>
2024-12-03 15:14:20 -0500

    Add files via upload

---
+++ exponentialBetting.py
@@ -0,0 +1,22 @@
+# Thank you proffesor Swihart
+import time
+from time import sleep
+import math
+from math import *
+import random
+money = 100
+
+while money>0:
+    coin = random.randint(0, 1)
+
+    k = 0
+    if coin == 1:
+        money = money +(2**k)
+        print(money)
+        k = 0
+        sleep(1)
+    else:
+        money = money - (2**k)
+        k = k+1
+        print(money)
+        sleep(1)
