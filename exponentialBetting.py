2356646463c81d14dc1bb4080b6e3cb99f28b7f0
John Paul <johnpaulx@icloud.com>
2024-12-08 16:56:19 -0500

    Update exponentialBetting.py

--- exponentialBetting.py
+++ exponentialBetting.py
@@ -9,15 +9,17 @@ money = 100 # sets the inital capital (you need alot of this for it to work in r
 k = 0
 bound = int(input("What bound do you want?"))
 while money>0: # basically this is a valid strategy while you still have some amount of money
-    coin = random.randint(0, 1) # makes a coin, 1 is heads, 0 is tails
+    coin = random.randint(0, bound) # makes a coin, 1 is a win state, else is a lose state 
+
+    # (discrete random variable)
 
 
-    if coin == 1: # you win if the coin lands on heads (or in this case 1)
+    if coin == 1: # you win if the coin is a 1
         money = money +(2**k) # adds to money
         print(money)
         k = 0 # restarts the exponent to 0
         sleep(1)
-    else: # you lose if the coin lands on tails (or in this case 0)
+    else: # you lose if the coin lands on any integer that is not 1
         money = money - (2**k) # subtracts from money
         k = k+1 # increases the exponent by 1 (after an infinite amount of time you are guaranteed to net gain one dollar)
         print(money)
