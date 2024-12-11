4f5be97b143e8708a61860114d9c1be67aa8c2a0
John Paul <johnpaulx@icloud.com>
2024-12-11 18:39:43 -0500

    Update collatzMarkov.py

--- collatzMarkov.py
+++ collatzMarkov.py
@@ -1,12 +1,15 @@
+# representing the collatz conjecture as a markov chain was an idea that came to me in a dream
+
+
 import math
 from math import *
 
-n = int(input("Enter a number"))
+n = int(input("Enter a number")) # this allows you to put in a number to test the collatz conjecture
 
 while n!=1:
     if n%2 == 0:
-        print("A")
-        n = n/2
+        print("A") # prints A if even
+        n = n/2 # then divides n by 2
     else:
-        print("B")
-        n = (3*n)+1
+        print("B") # prints B if odd
+        n = (3*n)+1 # then multiplies n by 3 and adds 1
