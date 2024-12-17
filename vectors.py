54eb67adb1eb97ece594fb1ca8e57eff58b62029
John Paul <johnpaulx@icloud.com>
2024-12-17 16:00:43 -0500

    Update vectors.py

--- vectors.py
+++ vectors.py
@@ -3,7 +3,8 @@ row1 = int(input("how many in row 1"))
 col1 = int(input("how many in column 1"))
 row2 = int(input("how many in row 2"))
 col2 = int(input("how many in column 1"))
-
+A=[]
+B=[]
 if row1 == row2 and col1 == col2:
     ASub = []
     print("Matrix A")
@@ -39,3 +40,11 @@ if row1 == row2 and col1 == col2:
 else:
     print("dot product is not computable, not same dimensions")
 print(dot)
+cross = []
+if row1 and row2 == 3 and col1 and col2 == 1:
+    cross.append(ASub[1]*BSub[2]-ASub[2]*BSub[1])
+    cross.append(ASub[2]*BSub[0]-ASub[0]*BSub[2])
+    cross.append(ASub[0]*BSub[1]-ASub[1]*BSub[0])
+    print("Cross product is ",end='')
+    print(cross)
+
