bda51aad9cd55182e39e18c418cdbcfb3173ec8e
John Paul <johnpaulx@icloud.com>
2025-01-12 14:24:07 -0500

    Update laplace.py

--- laplace.py
+++ laplace.py
@@ -1,6 +1,18 @@
+# we still don't know where this comes from
+
+
+
 import math
 from math import *
 
-def lapSuc(n,m):
-    return((n+1)/(n+m+1))
-print(lapSuc(2,3))
+def lapSuc(n,m): # function for laplace succesion, takes in number of wins and losses
+    return((n+1)/(n+m+1)) # simulates an extra loss and win and calculates the odds of winning the next trial
+print(lapSuc(2,3)) # prints the results
+
+
+
+
+
+
+
+# perhaps it came to me in a dream
