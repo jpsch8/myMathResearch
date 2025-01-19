68ef2d68ac19b9af905e1192e2d19aff44dd030a
John Paul <johnpaulx@icloud.com>
2025-01-19 11:59:43 -0500

    Update blackScholls.py

--- blackScholls.py
+++ blackScholls.py
@@ -6,7 +6,13 @@ def call(N,S,K,r,t,sigma):
     d2 = d1-sigma*(t**0.5)
     return N*d1*S-N*d2*K*(e**(-1*r*t))
 
+N = float(input("CDF of the normal distribution")
+S = float(input("spot price of an asset")
+K = float(input("strike price")
+r = float(input("risk-free interest rate")
+t = float(input("time to maturity")
+sigma = float(input("volatility of the asset")
+          
 
-
-
+print("Call price is ",end="")
 print(call(10,11,10,10,10,10))
