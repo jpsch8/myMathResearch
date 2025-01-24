77a4b8f01701792a134d23ed4d6cee3c5b222c50
John Paul <johnpaulx@icloud.com>
2025-01-23 19:15:41 -0500

    Update blackScholls.py

--- blackScholls.py
+++ blackScholls.py
@@ -1,11 +1,13 @@
 import math
 from math import *
 
-def call(N,S,K,r,t,sigma):
-    d1 = (math.log(S/K)+(r+((sigma)**2)/2)*t)/(sigma*(t**0.5))
-    d2 = d1-sigma*(t**0.5)
-    return N*d1*S-N*d2*K*(e**(-1*r*t))
+def call(N,S,K,r,t,sigma): # this function will give the call price
+    d1 = (math.log(S/K)+(r+((sigma)**2)/2)*t)/(sigma*(t**0.5)) # this calculates the first part of the equation
+    d2 = d1-sigma*(t**0.5) # this calculates the second part of the equation
+    return N*d1*S-N*d2*K*(e**(-1*r*t)) # this returns the rest of the equation finally calculating the call price
 
+
+# the following allows use to input the prametres : CDF of the normal distribution, spot price, strike price, risk-free interest rate, time to maturity and volatility
 N = float(input("CDF of the normal distribution")
 S = float(input("spot price of an asset")
 K = float(input("strike price")
@@ -15,4 +17,4 @@ sigma = float(input("volatility of the asset")
           
 
 print("Call price is ",end="")
-print(call(N,S,K,r,t,sigma))
+print(call(N,S,K,r,t,sigma)) # this calls the function to give call price
