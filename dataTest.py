0ebd91bfd860cb2a71c78aae5eccff40771cd6a2
John Paul <johnpaulx@icloud.com>
2025-02-02 11:32:10 -0500

    Add files via upload

---
+++ dataTest.py
@@ -0,0 +1,18 @@
+import math
+from math import *
+
+def centre(S):
+    x_coord = 0
+    y_coord = 0
+    for i in range(0,len(S)):
+        x_coord = x_coord + S[i][0]
+
+    for j in range(0, len(S)):
+        y_coord = y_coord + S[j][1]
+
+
+    return [x_coord/len(S),y_coord/len(S)]
+
+
+S = [[1,2],[2,3],[3,2],[4,6],[5,17]]
+print(centre(S))
