328cc8c33b00f7b95deff2b51c00639c9c632aa7
John Paul <johnpaulx@icloud.com>
2025-02-08 15:01:31 -0500

    Update dataTest.py

--- dataTest.py
+++ dataTest.py
@@ -16,3 +16,12 @@ def centre(S):
 
 S = [[1,2],[2,3],[3,2],[4,6],[5,17]]
 print(centre(S))
+
+
+def move(S,p):
+    new_S = []
+
+    for i in range(0,len(S)):
+        new_S.append([float(S[i][0]-p[0]),float(S[i][1]-p[1])])
+
+    return new_S
