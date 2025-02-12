8916e7b1fa56833ec2d200d46c2ec6e63ec369d5
John Paul <johnpaulx@icloud.com>
2025-02-12 18:41:13 -0500

    Update dataTest.py

--- dataTest.py
+++ dataTest.py
@@ -1,17 +1,18 @@
 import math
 from math import *
 
-def centre(S):
-    x_coord = 0
+def centre(S): # passes in an array of arrays ((x,y) coordinates of data)
+    # sets a each component to 0
+    x_coord = 0 
     y_coord = 0
     for i in range(0,len(S)):
-        x_coord = x_coord + S[i][0]
+        x_coord = x_coord + S[i][0] # adds up all the x components
 
     for j in range(0, len(S)):
-        y_coord = y_coord + S[j][1]
+        y_coord = y_coord + S[j][1] # adds up all the y components
 
 
-    return [x_coord/len(S),y_coord/len(S)]
+    return [x_coord/len(S),y_coord/len(S)] # returns an array that represents a singal point, the average of the x and y componenets respectively
 
 
 S = [[1,2],[2,3],[3,2],[4,6],[5,17]]
