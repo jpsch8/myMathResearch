68b8f2a6fbe354418775d5709b27840feac7ac4e
John Paul <johnpaulx@icloud.com>
2025-04-26 18:26:14 -0400

    Update tradingAlgorithms.py

--- tradingAlgorithms.py
+++ tradingAlgorithms.py
@@ -5,7 +5,7 @@ from time import *
 price = 50 # the price of the stock starts at 50
 money = 10000 # this is the intital amount of money to start with
 shares = 0 # starting at 0 shares
-for i in range(0,365): # the loop runs 365 times for 365 days (should probably change this)
+while True:
     change = random.randint(-3,3) # this is the discrete random variable for the stock price it can change by a maximum magnitude of 3
     price = price + change # this sets the new price
     print('money: $' + str(money))
