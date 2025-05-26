8552e2f94df3ce58b73b153eb0341481852bc44c
John Paul <johnpaulx@icloud.com>
2025-05-26 12:14:42 -0400

    Update diophantine.py

--- diophantine.py
+++ diophantine.py
@@ -2,12 +2,13 @@
 # x^2 + 3x + 9 = 9y^2
 
 def solve(x,y):
-    if(x**2 + 3*x + 9 - 9 * y**2 == 0):
-        print("(" + str(x) + ", " + str(y) + ")")
+    if(x**2 + 3*x + 9 - 9 * y**2 == 0): # checks to see if the integer pair satisfies the equation
+        print("(" + str(x) + ", " + str(y) + ")") # if it does, we print that pair to the console
 
 
 
     return 0
-for i  in range(-999,999):
-    for j in range(-999, 999):
+# this algorithm is terrible and needs adjustments
+for i  in range(-999,999): # checks every x value on [-999,999]
+    for j in range(-999, 999): # checks every y value on [-999,999]
         solve(i,j)
